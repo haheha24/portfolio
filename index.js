213fac60aefe8f9e6396d10d3c600b84c70be018
haheha24 <haheha24@users.noreply.github.com>
2021-05-04 16:04:51 +1000

    fixed scroll button and made responsive

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ function scrollFunction() {
 }
 
 // When the user clicks on the button, scroll to the top of the document
-function topFunction() {
+function topButton() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+// SCROLLING BACK TO TOP ISN'T WORKING
