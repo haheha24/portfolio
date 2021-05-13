cd35eea2b20ef61a4b6ce7a4c018a81cbd758a32
haheha24 <haheha24@users.noreply.github.com>
2021-05-13 22:03:33 +1000

    split the nav position to left and right

--- index.js
+++ index.js
@@ -18,4 +18,3 @@ function topButton() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
-// SCROLLING BACK TO TOP ISN'T WORKING
