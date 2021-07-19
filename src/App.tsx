7241f0c3e857aa9bedc4d495ca30940a42e95a20
haheha24 <haheha24@users.noreply.github.com>
2021-07-19 22:55:03 +1000

    fixed the workings, wasn't paying attention to the relative path

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import './App.css';
-import Header from './src/components/header/Header'
-import Navbar from './src/components/nav/Navbar'
-import Projects from './src/components/projects/Projects'
-import Footer from './src/components/footer/Footer'
+import Header from './components/header/Header'
+import Projects from './components/projects/Projects'
+import Footer from './components/footer/Footer'
 
 
 
 function App() {
   return (
     <div className="App">
-   
+   <Header />
+  <Projects />
+  <Footer />
     </div>
   );
 }
