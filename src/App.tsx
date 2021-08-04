982a4eb4b393605dd07037f1c87a67e3cad2f423
haheha24 <haheha24@users.noreply.github.com>
2021-08-04 22:41:54 +1000

    added completed boolean and stars number to 'database' and fixed up typing

--- src/App.tsx
+++ src/App.tsx
@@ -3,32 +3,37 @@ import Main from "./components/main/Main";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import React, { useEffect, useState } from "react";
-import ProjectData from './projects'
+import ProjectData from "./projects";
+import { IconContext } from "react-icons/lib";
 
 export type Project = {
-  id: string | number
-  title: String;
-  desc: String;
+  id: string | number;
+  title: string;
+  desc: string;
   repoLink: string;
   deployLink: string;
-  tags: String[];
+  tags: string[];
+  completed: boolean;
+  stars: number;
 };
 
 export const ProjectContext = React.createContext<Project[]>([]);
 
 function App() {
-  const [projectState, setProjectState] = useState<Project[]>([])
-  
+  const [projectState, setProjectState] = useState<Project[]>([]);
+
   useEffect(() => {
-    return setProjectState([...ProjectData])
-  }, [])
+    return setProjectState([...ProjectData]);
+  }, []);
 
   return (
     <div className="grid-container">
       <ProjectContext.Provider value={projectState}>
-        <Header />
-        <Main />
-        <Footer />
+        <IconContext.Provider value={{ className: "react-icons" }}>
+          <Header />
+          <Main />
+          <Footer />
+        </IconContext.Provider>
       </ProjectContext.Provider>
     </div>
   );
--- src/projects.tsx
+++ src/projects.tsx
@@ -1,74 +1,73 @@
-const projectData = 
-  [
-    {
-      "id": "1",
-      "title": "CheckList",
-      "desc": "This is a single page Vanilla JavaScript project on GitHub",
-      "repoLink": "https://github.com/haheha24/CheckList",
-      "deployLink": "https://haheha24.github.io/CheckList/",
-      "tags": ["JavaScript", "HTML", "CSS"],
-      "completed": "false",
-      "stars": "2"
-    },
-    {
-      "id": "2",
-      "title": "Lookup",
-      "desc": "This is a single page MERN stack project on GitHub",
-      "repoLink": "https://github.com/haheha24/lookup-repo",
-      "deployLink": "N/A",
-      "tags": [
-        "JavaScript",
-        "React",
-        "Node.JS",
-        "Express.JS",
-        "Mongoose.JS",
-        "MongoDB Atlas",
-        "HTML",
-        "CSS",
-      ],
-      "completed": "false",
-      "stars": "2"
-    },
-    {
-      "id": "3",
-      "title": "FCC: Technical Documentation Page",
-      "desc": "This is a single page JavaScript project done on my codepen.io",
-      "repoLink": "https://codepen.io/haheha24/pen/VwPdxeB",
-      "deployLink": "",
-      "tags": ["HTML", "CSS", "JavaScript"],
-      "completed": "true",
-      "stars": "3"
-    },
-    {
-      "id": "4",
-      "title": "Product Landing Page",
-      "desc": "This is a single page Product Landing Page project done on my codepen.io",
-      "repoLink": "https://codepen.io/haheha24/pen/poRVGZQ",
-      "deployLink": "",
-      "tags": ["HTML", "CSS"],
-      "completed": "true",
-      "stars": "3"
-    },
-    {
-      "id": "5",
-      "title": "FCC: Survey Form",
-      "desc": "This is a single page Survey Form project done on my codepen.io",
-      "repoLink": "https://codepen.io/haheha24/pen/LYxmYVL",
-      "deployLink": "",
-      "tags": ["HTML", "CSS"],
-      "completed": "true",
-      "stars": "3"
-    },
-    {
-      "id": "6",
-      "title": "FCC: Tribute Page",
-      "desc": "This is a single page FCC: Tribute Page project done on my codepen.io",
-      "repoLink": "https://codepen.io/haheha24/pen/XWpMxqL",
-      "deployLink": "",
-      "tags": ["HTML", "CSS"],
-      "completed": "true",
-      "stars": "3"
-    }
-  ];
+const projectData = [
+  {
+    id: "1",
+    title: "CheckList",
+    desc: "This is a single page Vanilla JavaScript project on GitHub",
+    repoLink: "https://github.com/haheha24/CheckList",
+    deployLink: "https://haheha24.github.io/CheckList/",
+    tags: ["JavaScript", "HTML", "CSS"],
+    completed: false,
+    stars: 2,
+  },
+  {
+    id: "2",
+    title: "Lookup",
+    desc: "This is a single page MERN stack project on GitHub",
+    repoLink: "https://github.com/haheha24/lookup-repo",
+    deployLink: "N/A",
+    tags: [
+      "JavaScript",
+      "React",
+      "Node.JS",
+      "Express.JS",
+      "Mongoose.JS",
+      "MongoDB Atlas",
+      "HTML",
+      "CSS",
+    ],
+    completed: true,
+    stars: 2,
+  },
+  {
+    id: "3",
+    title: "FCC: Technical Documentation Page",
+    desc: "This is a single page JavaScript project done on my codepen.io",
+    repoLink: "https://codepen.io/haheha24/pen/VwPdxeB",
+    deployLink: "",
+    tags: ["HTML", "CSS", "JavaScript"],
+    completed: true,
+    stars: 1,
+  },
+  {
+    id: "4",
+    title: "Product Landing Page",
+    desc: "This is a single page Product Landing Page project done on my codepen.io",
+    repoLink: "https://codepen.io/haheha24/pen/poRVGZQ",
+    deployLink: "",
+    tags: ["HTML", "CSS"],
+    completed: true,
+    stars: 1,
+  },
+  {
+    id: "5",
+    title: "FCC: Survey Form",
+    desc: "This is a single page Survey Form project done on my codepen.io",
+    repoLink: "https://codepen.io/haheha24/pen/LYxmYVL",
+    deployLink: "",
+    tags: ["HTML", "CSS"],
+    completed: true,
+    stars: 1,
+  },
+  {
+    id: "6",
+    title: "FCC: Tribute Page",
+    desc: "This is a single page FCC: Tribute Page project done on my codepen.io",
+    repoLink: "https://codepen.io/haheha24/pen/XWpMxqL",
+    deployLink: "",
+    tags: ["HTML", "CSS"],
+    completed: true,
+    stars: 1,
+  },
+];
 
-export default projectData
+export default projectData;
