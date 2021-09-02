aa52452846182c4ccc43c8be1b92e80ebaa7abbd
haheha24 <haheha24@users.noreply.github.com>
2021-09-03 00:45:18 +1000

    created a temporary filter function that only works for completed and not completed. In filterFunction, checBoxTrueArray filters all items from checkBoxList and returns the ones only that are checked: true. After checking that checkBoxTrueArray.length > 0, checkBoxFilter is my way of destructuring the arrays while looping within the length of projects. Otherwise looping through each checkBoxTrueArray while it's length is less than projects will cause a compile failure as checked will be undefined. By creating a constant for each checkbox name that applies checkBoxFilter, I can safely filter each project against the checkbox condition. I have found a O(n^2) algorithm on stackoverflow that helps remove duplicates while I concatenate each of the different new filtered projects. However the return value of the final filter is unsuccessful as it will ALWAYS concatenate, even if there is only one box checked. Should be a relatively easy fix by checking the length of checkBoxTrueArray. Another issue is being able to dynamically add the necessary filtered projects before the final merge.

--- src/components/pages/projects/projectForm/FilterFunctions.tsx
+++ src/components/pages/projects/projectForm/FilterFunctions.tsx
@@ -1,5 +1,10 @@
 import { Project } from "../../../../App";
 
+interface IcheckBoxList {
+  checkBox: string;
+  checked: boolean;
+}
+
 export const searchFunction = (
   searchValue: string,
   projects: Project[]
@@ -18,16 +23,59 @@ export const searchFunction = (
 export const filterFunction = (
   value: string,
   projects: Project[],
-  checkBoxArray: string | any
+  checkBoxList: IcheckBoxList[]
 ): Array<Project> => {
-  const filteredState = projects.filter((stateSearch) => {
-    return (
-      stateSearch.completed === true /* ||
-       stateSearch.desc.toLowerCase().match(checkBoxArray[1]) ||
-      stateSearch.deployLink.toLowerCase().match(checkBoxArray[1]) ||
-      stateSearch.repoLink.toLowerCase().match(checkBoxArray[1]) ||
-      stateSearch.tags.toString().toLowerCase().match(checkBoxArray[1]) */
-    );
+  //Filter any checkboxes that are true
+  const checkBoxTrueArray = checkBoxList.filter((checkBoxTrue) => {
+    return checkBoxTrue.checked === true;
   });
-  return searchFunction(value, projects);
+  if (checkBoxTrueArray.length === 0) {
+    return searchFunction(value, projects);
+  } else {
+    const checkBoxFilter = (
+      checkBoxArray: IcheckBoxList[],
+      checkBoxName: string
+    ) => {
+      let checkBoxFiltered = checkBoxArray.filter((checkBoxItem) => {
+        if (
+          checkBoxItem.checkBox === checkBoxName &&
+          checkBoxItem.checked === true
+        ) {
+          return checkBoxItem.checkBox === checkBoxName;
+        } else {
+          return checkBoxItem;
+        }
+      });
+      //The filtered checkBoxList[] will always have 1 key in it. This means returning checkBoxFiltered[0] allows us to directly access
+      //the checkBox object and it's properties from the "array".
+      return checkBoxFiltered[0];
+    };
+    //Make a constant for each filtered checkbox
+    const completed = checkBoxFilter(checkBoxList, "Completed"); // === true for completed and !== for not completed
+    const notCompleted = checkBoxFilter(checkBoxList, "Not completed");
+    console.log(
+      `This is completed: ${completed} and this is not completed: ${notCompleted}`
+    );
+    //make a filtered project for each type of checkbox filter.
+    const completedProjects = projects.filter((project) => {
+      return completed.checked === project.completed;
+    });
+    const notCompletedProjects = projects.filter((project) => {
+      return notCompleted.checked !== project.completed;
+    });
+    // Concatenate all filtered project arrays into a single return value for searchFunction
+    const mergeProjects = (args: Project[]) => {
+      let a = args.concat()
+      for(let i=0; i < a.length; ++i) {
+        for(let j=i+1; j < a.length; ++j) {
+          if(a[i] === a[j]) {
+            a.splice(j--, 1)
+          }
+        }
+      }
+      return a
+    } ;
+    const filteredProjects = mergeProjects(completedProjects.concat(notCompletedProjects))
+    return searchFunction(value, filteredProjects);
+  }
 };
