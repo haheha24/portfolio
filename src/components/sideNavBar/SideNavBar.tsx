36245536d4cf5eb0c32e42dc794ca8084f1fdf68
haheha24 <haheha24@users.noreply.github.com>
2021-09-25 01:52:04 +1000

    made sidenavbar responsive with a menu button at 860px width or less. touched up some CSS background hovering over both navigation menus. It is not perfect but it is on the right track and is working more than great for now. Still thinking of refactoring useMediaQuery and pass it from app through context to both sidenav and navbar, rather than repeating the importing and calculations twice in each component.

--- src/components/sideNavBar/SideNavBar.tsx
+++ src/components/sideNavBar/SideNavBar.tsx
@@ -1,30 +1,56 @@
 import "./sidenavbar.css";
+import useMediaQuery from "../hooks/useMediaQuery";
+import React, { useEffect, useRef } from "react";
 import { IhomeSubtitle } from "../pages/home/Home";
 import { Link } from "react-scroll";
+import { GiHamburgerMenu } from "react-icons/gi";
 
-const SideNavBar = (props: {
-  navArray: IhomeSubtitle[];
-}) => {
+const SideNavBar = (props: { navArray: IhomeSubtitle[] }) => {
+  const sideMediaQuery = useMediaQuery().width;
+  const sideNavRef = useRef<HTMLElement>(null);
+  const sideBurgerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    let newNavRef = sideNavRef;
+    let newBurgerRef = sideBurgerRef;
+    if (sideMediaQuery <= 860 && newNavRef !== null) {
+      newBurgerRef.current?.addEventListener("click", () => {
+        newNavRef.current?.classList.toggle("displayBlock");
+        return () => newNavRef.current?.classList.toggle("displayBlock");
+      });
+      return () =>
+        newBurgerRef.current?.addEventListener("click", () => {
+          newNavRef.current?.classList.toggle("displayBlock");
+          return () => newNavRef.current?.classList.toggle("displayBlock");
+        });
+    }
+  }, [sideMediaQuery]);
+
   return (
-    <nav id="sidenav">
-      <ul className="sidenav-ul">
-        {props.navArray.map((navItem: IhomeSubtitle, index) => {
-          return (
-            <li key={index} className="sidenav-item">
-              <Link
-                activeClass="sidenav-current"
-                className="sidenav-item-anchor"
-                to={navItem.itemId}
-                spy={true}
-                offset={-77.5}
-              >
-                {navItem.itemName}
-              </Link>
-            </li>
-          );
-        })}
-      </ul>
-    </nav>
+    <aside className="sidenav-container">
+      <div className="sidenav-burger" ref={sideBurgerRef}>
+        <GiHamburgerMenu style={{ width: "2.5em", height: "2.5em" }} />
+      </div>
+      <nav className="sidenav" ref={sideNavRef}>
+        <ul className="sidenav-ul" >
+          {props.navArray.map((navItem: IhomeSubtitle, index) => {
+            return (
+              <li key={index} className="sidenav-item">
+                <Link
+                  activeClass="sidenav-current"
+                  className="sidenav-item-anchor"
+                  to={navItem.itemId}
+                  spy={true}
+                  offset={-77.5}
+                >
+                  {navItem.itemName}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    </aside>
   );
 };
 
