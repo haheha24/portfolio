f9792767177376b86c035bc90adb6e5374a4ba40
haheha24 <haheha24@users.noreply.github.com>
2021-11-13 17:49:07 +1100

    followed a guide to help create a secure oauth2 connection with my gmail account however the token's username and pw are incorrect. Will investigate. Nodemailer.createTransport kept having a typing error because it was looking for a type that would match transportOptions, which there wasn't. extending the typing to SMTPTransport.TransportOptions has seem to satisfy this for now until I further understand the typing of nodemailer. Currently the goal to get the oauth connection going.

--- server/src/router/contactEmail.ts
+++ server/src/router/contactEmail.ts
@@ -1,4 +1,62 @@
+require('dotenv').config()
 import express from 'express'
+import nodemailer from 'nodemailer'
+import SMTPTransport from "nodemailer";
+import { google } from 'googleapis'
+
+//initialise Oauth2 through googleapi
+const OAuth2 = google.auth.OAuth2;
+
+//create the transporter that will create the nodemailer transport object
+const createTransporter = async () => {
+    //create a new oauth2 client using our client id and client secret from the api
+    const oauth2Client = new OAuth2(
+        process.env.CLIENT_ID,
+        process.env.CLIENT_SECRET,
+        "https://developers.google.com/oauthplayground"
+    )
+
+    //grabs the refresh token
+    oauth2Client.setCredentials({
+        refresh_token: process.env.REFRESH_TOKEN
+    })
+
+    //create a new promise that will get the accesstoken. can't use async await sugar
+    const accessToken = await new Promise((resolve, reject) => {
+        oauth2Client.getAccessToken((err, token) => {
+            if (err) {
+                reject("Failed to create access token");
+            }
+            resolve(token);
+        });
+    });
+
+    //create the actual transporter object 
+    const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            type: "OAuth2",
+            user: process.env.EMAIL,
+            accessToken,
+            clientId: process.env.CLIENT_ID,
+            clientSecret: process.env.CLIENT_SECRET,
+            refreshToken: process.env.REFRESH_TOKEN
+        },
+    } as SMTPTransport.TransportOptions);
+
+    return transporter
+}
+
+
+const sendEmail = async (emailOptions: {}) => {
+    let emailTransporter = await createTransporter()
+    try {
+        await emailTransporter.sendMail(emailOptions)
+    } catch (error) {
+        console.log(error)
+    }
+
+}
 
 const contactEmailRouter = express.Router()
 contactEmailRouter.use(express.json())
@@ -6,7 +64,8 @@ contactEmailRouter.use(express.urlencoded({ extended: true }));
 
 contactEmailRouter.post("/", (req, res) => {
     const contactEmail = req.body
-    res.send(`received ${contactEmail}`)
+    sendEmail(contactEmail)
+    res.json(contactEmail)
 })
 
 contactEmailRouter.get("/", (req, res) => {
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,25 +1,7 @@
 import Express from 'express'
-/* import nodemailer from 'nodemailer' */
 import router from './router/index'
 
 const app = Express();
-
-//Nodemailer
-/* let transporter = nodemailer.createTransport();
-
-let message = {
-    from: "sender",
-    to: "adriancristallo1@gmail.com",
-    subject: "subject",
-    text: "enquiry",
-    html: "<p>HTML version of the message</p>"
-} */
-
-//Temp get
-app.get("/", (req, res) => {
-    res.send("hello"),
-    console.log("hello")
-})
 
 app.use('/router', router)
 
