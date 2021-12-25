a4ea0b5c5833142adb40476cd30efb2846efb3be
haheha24 <haheha24@users.noreply.github.com>
2021-12-25 23:19:51 +1100

    reconfigured npm scripts in root and server folders. More streamline and control in accessing the sub folder scripts from the root. Also added more configuration to server/tsconfig.json. These decisions were made because of the benefit of greater control of npm scripts, as well as more utility for tracking separate terminal logs if needed be for debugging and development.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,29 @@
-import Express from 'express'
-import router from './router/index'
+import Express from "express";
+import router from "./router/index";
+import path from "node:path";
 
 const app = Express();
 
-app.use('/router', router)
+// Serve React static files
+//Set the base url to serve static content in this directory
+app.use(Express.static(path.join(__dirname, "../../../client/build")));
+
+//a get request to serve the index.html file of the project
+app.get("*", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "../../../client/build", "/index.html"),
+    (err) => {
+      if (err) {
+        res.status(500).send(err);
+      }
+    }
+  );
+});
+
+app.use("/router", router);
 
 //Port
 const port = 5000;
 app.listen(port, () => {
-    console.log(`listening on port: ${port}`)
-})
+  console.log(`listening on port: ${port}`);
+});
