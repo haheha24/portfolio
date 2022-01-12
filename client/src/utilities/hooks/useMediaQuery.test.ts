ba565e10118306bcef7597112583d0a2af8fd982
haheha24 <haheha24@users.noreply.github.com>
2022-01-12 23:45:37 +1100

    Created tests that check the performance of the useMediaQuery by utilising react-hooks testing lib. Had to create a custom resize function that alters the window innerWidth and innerHeight properties with Object.defineproperty. This is because JSdom window global does not contain the method resizeTo()

---
+++ client/src/utilities/hooks/useMediaQuery.test.ts
@@ -0,0 +1,41 @@
+import useMediaQuery from "./useMediaQuery";
+import { act, renderHook } from "@testing-library/react-hooks";
+
+/**
+ * JSDom doesn't have a method for window.resizeTo() so defining the values properties in innerX in window hacks around it.
+ * This function would not resize the actual window in the browser DOM but in
+ * JSDom it mocks it by grabbing redefining the window properties innerWidth and innerHeight.
+ * These are the same values of what window.resizeTo() would mutate.
+ */
+const customResize = (width: number, height: number) => {
+  Object.defineProperty(window, "innerWidth", {
+    writable: true,
+    configurable: true,
+    value: width,
+  });
+  Object.defineProperty(window, "innerHeight", {
+    writable: true,
+    configurable: true,
+    value: height,
+  });
+  return;
+};
+
+const { rerender, result } = renderHook(() => useMediaQuery());
+
+describe("useMediaQuery hook tests", () => {
+  it("get the initial width and height of the window", () => {
+    expect(result.current.width).toBe(window.innerWidth);
+    expect(result.current.height).toBe(window.innerHeight);
+  });
+  it("resize the window and call useMediaQuery", () => {
+    expect(result.current.width).toBe(window.innerWidth);
+    expect(result.current.height).toBe(window.innerHeight);
+    act(() => {
+      customResize(860, 768);
+      rerender();
+    });
+    expect(result.current.width).toBe(860);
+    expect(result.current.height).toBe(768);
+  });
+});
--- client/src/utilities/hooks/useMediaQuery.tsx
+++ client/src/utilities/hooks/useMediaQuery.tsx
@@ -17,11 +17,6 @@ const useMediaQuery = () => {
     const getWindowDimension = () => {
       let { innerHeight: height, innerWidth: width } = window;
       setWindowDimension({ height, width });
-      let time = Date.now();
-      let newTime = time + 1000 - Date.now();
-      if (newTime < 0) {
-        time = Date.now();
-      }
       return windowDimension;
     };
     window.addEventListener("resize", _(getWindowDimension, 500));
