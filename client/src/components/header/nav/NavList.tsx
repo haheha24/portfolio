6d7d0eb5ca7868b5eb082e8f960f5e29699d7120
haheha24 <haheha24@users.noreply.github.com>
2022-01-15 16:32:06 +1100

    removed activeClass prop from NavLink in preparation for when react-router-dom updates to v6 officially

--- client/src/components/header/nav/NavList.tsx
+++ client/src/components/header/nav/NavList.tsx
@@ -1,28 +1,36 @@
 import { NavLink } from "react-router-dom";
 
 export interface INavLinkParams {
-  liClass: string,
-  listName: string,
-  endPoint: string,
-  navLinkId: string,
-  navLinkClass: string,
-  navLinkActive: string,
+  liClass: string;
+  listName: string;
+  endPoint: string;
+  navLinkId: string;
+  navLinkClass: string;
+  navLinkActive: string;
 }
 
-const NavList = ({liClass, listName, endPoint, navLinkId, navLinkClass, navLinkActive}: INavLinkParams) => {
+const NavList = ({
+  liClass,
+  listName,
+  endPoint,
+  navLinkId,
+  navLinkClass,
+  navLinkActive,
+}: INavLinkParams) => {
   return (
     <li className={liClass}>
-            <NavLink
-              to={endPoint}
-              id={navLinkId}
-              className={navLinkClass}
-              activeClassName={navLinkActive}
-              exact={true}
-            >
-              {listName}
-            </NavLink>
-          </li>
-  )
-}
+      <NavLink
+        to={endPoint}
+        id={navLinkId}
+        className={(isActive) =>
+          navLinkClass + " " + (!isActive ? "unselected" : navLinkActive)
+        }
+        exact={true}
+      >
+        {listName}
+      </NavLink>
+    </li>
+  );
+};
 
-export default NavList
+export default NavList;
