e0df4e9000c1edd6447848371410864cc009bd73
haheha24 <haheha24@users.noreply.github.com>
2022-07-14 22:18:51 +1000

    refactored how image paths resolve by storing the images in the public folder as static files

--- client/src/components/main/projects/Projects.tsx
+++ client/src/components/main/projects/Projects.tsx
@@ -1,20 +1,19 @@
 import "./projects.css";
 import ProjectCard from "./ProjectCard";
-import lookUp from "../../../../images/lookup-project-herokuapp.png";
 
 const Projects = () => {
   return (
     <>
-        <div className="projects-container">
-          <ProjectCard
-            url="https://lookup-project.herokuapp.com/"
-            github="https://github.com/haheha24/lookup-repo"
-            image={lookUp}
-            altTxt="Look Up Project"
-            title="Look Up Project"
-            description="This is a test card for look up."
-          />
-        </div>
+      <div className="projects-container">
+        <ProjectCard
+          url="https://lookup-project.herokuapp.com/"
+          github="https://github.com/haheha24/lookup-repo"
+          image={"http://localhost:3000/images/lookup-project-herokuapp.png"}
+          altTxt="Look Up Project"
+          title="Look Up Project"
+          description="This is a test card for look up."
+        />
+      </div>
     </>
   );
 };
