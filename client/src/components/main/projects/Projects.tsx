30d940f5f921d741369042e07e2f034580e92455
haheha24 <haheha24@users.noreply.github.com>
2022-07-16 17:21:55 +1000

    the organisation of cards is more adaptable per viewport breakpoint. Also populated actual project cards with my projects

--- client/src/components/main/projects/Projects.tsx
+++ client/src/components/main/projects/Projects.tsx
@@ -6,12 +6,28 @@ const Projects = () => {
     <>
       <div className="projects-container">
         <ProjectCard
-          url="https://lookup-project.herokuapp.com/"
-          github="https://github.com/haheha24/lookup-repo"
-          image={"http://localhost:3000/images/lookup-project-herokuapp.png"}
-          altTxt="Look Up Project"
-          title="Look Up Project"
-          description="This is a test card for look up."
+          url=""
+          github="https://github.com/haheha24/portfolio"
+          image={`/images/portfolio.png`}
+          altTxt="Portfolio Feature Image"
+          title="Portfolio"
+          description="My portfolio website you are currently viewing."
+        />
+        <ProjectCard
+          url="https://mysticcoven.com.au/"
+          github=""
+          image={`/images/mysticcoven.png`}
+          altTxt="Mystic Coven Feature Image"
+          title="Mystic Coven"
+          description="A business website for Mystic Coven. Customised Wordpress theme of Elegant Fashion by Rara Themes."
+        />
+        <ProjectCard
+          url=""
+          github="https://github.com/haheha24/planner-app"
+          image={``}
+          altTxt="Work in progress"
+          title="Planner App"
+          description="A personalised planning app that can integrate with your email."
         />
       </div>
     </>
