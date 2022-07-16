fe0427cd9a824ca36530310f6f9af30876026677
haheha24 <haheha24@users.noreply.github.com>
2022-07-17 02:17:42 +1000

    Refactored thank you message to be more clear with better UI

--- client/src/components/main/contact/Thankyou.tsx
+++ client/src/components/main/contact/Thankyou.tsx
@@ -2,9 +2,9 @@ import "./thankyou.css";
 const Thankyou = () => {
   return (
     <section className="thankyou">
-      <p>
+      <p className="thankyou-paragraph">
         Thank you for your enquiry. <br />A confirmation email will be sent
-        and will be responded to within 48 hours.
+        and your email will be responded to within 48 hours.
       </p>
     </section>
   );
