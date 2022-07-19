0ffa4b6d3e078253ecf1c368eb6dc16558207079
haheha24 <haheha24@users.noreply.github.com>
2022-07-19 17:07:47 +1000

    removed the static files connected through middleware. Both client and server modules are stand alone and can run separately in their respective terminals. Moreover, they are both being deployed separately to heroku (server) and netlify (client) so serving the client as a file on particular end points is redundant. This has all stemmed from the inability to reliable determine the NODE_ENV without manually setting it and messing about with NODE itself.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,61 +1,10 @@
 import Express from "express";
 import router from "./router/index";
-import path from "node:path";
 
 const app = Express();
 
 // Routes index
 app.use("/router", router);
-
-// Serve React static files
-// Set the base url to serve static content in this directory
-app.use(Express.static(path.join(__dirname, "../../../client/build")));
-app.use(Express.static(path.join(__dirname, "../../../client/public")));
-
-if (process.env.NODE === "development") {
-  // a wildcard for all get requests to serve the index.html file of the project
-  //Only intercepting get requests incase development wants to test other requests.
-  app.get("*", (req, res) => {
-    if (
-      req.url === "/" ||
-      req.url === "/about" ||
-      req.url === "/projects" ||
-      req.url === "/contact"
-    ) {
-      res.sendFile(
-        path.join(__dirname, "../../../client/build", "/index.html"),
-        (err) => {
-          if (err) {
-            res.status(500).send(err);
-          }
-        }
-      );
-    } else {
-      res.status(404).send("Error 404: Page does not exist.");
-    }
-  });
-} else {
-  // a wildcard for all requests to serve the index.html file of the project
-  app.all("*", (req, res) => {
-    if (
-      req.url === "/" ||
-      req.url === "/about" ||
-      req.url === "/projects" ||
-      req.url === "/contact"
-    ) {
-      res.sendFile(
-        path.join(__dirname, "../../../client/build", "/index.html"),
-        (err) => {
-          if (err) {
-            res.status(500).send(err);
-          }
-        }
-      );
-    } else {
-      res.status(404).send("Error 404: Page does not exist.");
-    }
-  });
-}
 
 // Port
 app.listen(process.env.PORT || 5000);
