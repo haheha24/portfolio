e637644dcd30f0092ce5948551a3f193759660e2
haheha24 <haheha24@users.noreply.github.com>
2022-07-20 18:51:41 +1000

    Readded the logo as part of item 1 - thecreativeage. Had to set the vertical-align to text-bottom as any other value would affect the alignment of each menu item.

--- client/src/components/header/nav/NavList.tsx
+++ client/src/components/header/nav/NavList.tsx
@@ -1,6 +1,7 @@
 import { ReactChild } from "react";
 import { NavLink } from "react-router-dom";
 import { scrollToElement } from "../../../utilities/helpers";
+import imgSiteLogo from "../../images/site_logo_512x512.png";
 
 export interface INavLinkParams {
   liClass: string;
@@ -36,7 +37,24 @@ const NavList = ({
             scrollToElement(`${navLinkId}Link`);
           }}
         >
-          {listName}
+          {listName === "theCreativeAge" ? (
+            <>
+              <img
+                src={imgSiteLogo}
+                alt="theCreativeAge site logo"
+                className="site-logo"
+                style={{
+                  color: "white",
+                  verticalAlign: "text-bottom",
+                  width: "1em",
+                  height: "1em"
+                }}
+              />
+              {listName}
+            </>
+          ) : (
+            listName
+          )}
         </NavLink>
       )}
     </li>
